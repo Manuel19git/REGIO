ceea845ec98873a0401f6581b2f4a60ab5dfb337
Manuel <manuelshino@gmail.com>
2023-03-28 08:57:16 +0200

    Added header of a class which is going to have direct3D content

--- REGIO/graphicsclass.h
+++ REGIO/graphicsclass.h
@@ -8,7 +8,7 @@
 //////////////
 // INCLUDES //
 //////////////
-#include <windows.h>
+#include "d3dclass.h"
 
 
 /////////////
@@ -37,6 +37,8 @@ private:
 	bool Render();
 
 private:
+
+	D3DClass* m_D3D;
 
 };
 
